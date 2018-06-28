6b1ee8ecd6eb1d8d43f996a33794fd3eb7fbad3e
Drew Ardner <ardnerdrew@gmail.com>
2018-06-28 14:40:18 -0400

    updated app component with references and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FABComponent } from './fab/fab.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FABComponent
   ],
   imports: [
     BrowserModule,
