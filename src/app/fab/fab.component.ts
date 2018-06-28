39ad8b8fce51b9ef8816778352f201b13b0f0261
Drew Ardner <ardnerdrew@gmail.com>
2018-06-28 14:39:19 -0400

    Created FAB component

---
+++ src/app/fab/fab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fab',
+  templateUrl: './fab.component.html',
+  styleUrls: ['./fab.component.scss']
+})
+export class FABComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
